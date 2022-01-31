39721e425cffa10a0550c38e3bdb4bc69fc0d5e0
t-akuya <bankband1990@gmail.com>
2022-01-31 15:15:37 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-
-         
+        
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: {minimum: 6 }
-  validates :password_confirmation, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
