92c992c4682cb5f3454573d165d00a152df2d136
t-akuya <bankband1990@gmail.com>
2022-02-13 11:41:07 +0900

    ActiveHashの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.all
+  end
 end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image)
+  end
+
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions  
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery
+  belongs_to :prefecture
+  belongs_to :shipment_days
+
 end
