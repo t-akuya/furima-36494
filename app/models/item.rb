a1961906386bfb04a115b55856303fb24025c2a9
t-akuya <bankband1990@gmail.com>
2022-02-13 13:55:58 +0900

    バリデーションの設定2

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,13 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipment_days
 
-  validates :name, :price, :text, :user presence: true
+  with_options presence: true do
+    validates :name
+    validates :price, format:       { with: /\A[0-9]+\z/ },
+                      numericality: { in:300..9999999 }
+    validates :text
+    validates :user
+  end
 
   validates :category_id, :condition_id, :delivery_id,
             :prefecture_id, :shipment_days_id, numericality: { other_than: 1 , message: "can't be blank" }
